23b2241874d7efe39d3f116560cb99205baac1f2
Keith Getty <keithgetty@yahoo.com>
2017-09-14 23:06:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,9 @@
 var playlist = {
   "The Police": "Every Breath You Take"
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+  }
+  
